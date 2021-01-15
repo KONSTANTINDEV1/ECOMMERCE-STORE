9a14bd093ec2a2ee48fbf9cf202708d62e42cb83
Tino_taylor <tino8music@gmail.com>
2021-01-15 18:36:44 +0100

    Sign in page and sign in form created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
+import Header from "./components/Header";
+import SignInSignUp from "./components/SignInSignUp";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUp} />
       </Switch>
     </div>
   );
