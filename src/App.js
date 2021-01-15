3cbc4afe26ef26d530aea395c440d50c5eb83ff5
Tino_taylor <tino8music@gmail.com>
2021-01-15 12:23:10 +0100

    Collections Preview foundations built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-import HomePage from "./pages/HomePage";
 import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/shop/ShopPage";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
